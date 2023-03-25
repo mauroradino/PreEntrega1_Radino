7c2f9eab970db1e3d70f0d2bee8a7a7a63ca675b
Mauro Radino <mauroradino2003@gmail.com>
2023-03-25 20:40:57 -0300

    terminado

---
+++ main.js
@@ -0,0 +1,20 @@
+let totalAlumnos
+let presentes = 0
+let ausentes = 0
+let ingresados = 1
+let i
+
+totalAlumnos = Number(prompt("determine la cantidad total de alumnos"))
+
+    for(let ingresados = 1;ingresados <= totalAlumnos;ingresados++){
+      i = prompt(`el alumno ${ingresados} esta (P) presente o (A) ausente?`)
+      if(i == 'P'){
+        presentes++
+      }
+      else if (i == 'A'){
+        ausentes++
+      }}
+      let presentismo = (presentes * 100)/totalAlumnos
+    console.log(`hay ${presentes} alumnos presentes`)
+    console.log(`hay ${ausentes} alumnos ausentes`)
+    console.log(`hay ${presentismo}% de presentismo`)
